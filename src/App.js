2c940a342a70c6a89878df720b99a3d3794bbb5d
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-10-10 23:20:02 +0530

    proh

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+const [state,setState] =React.useState([])
+const changEvent=(e)=>{
+ setState(e.target.value)
+}
+const pt={paddingTop:"10px"}
+const arr=Array.from(state)
+  return (<>
+    <textarea style={{width:'80%',height:'30%'}} type="text" value={state} onChange={changEvent}/>
+    <div>
+    <h1>
+      containt
+    </h1>
+    <div style={{display:"flex"}}> 
+    {arr.map((e)=>{
+      if(e===" ")
+      {
+        return (
+          <div style={{width:"10px"}}>
+            </div>
+          
+        )
+        
+      }
+      if(e===e.toUpperCase())
+      {
+        return (
+          <div>
+          <img key={e.length} src={`${e}.png`} alt={e} height='20'/>
+          </div>
+        )
+      }
+      
+      if(e===e.toLowerCase()){
+        if(e==='y'||e==='g'||e==='p'||e==='q'||e==='z')
+        {
+          return (
+            <div>
+            <img key={e.length} src={`smallcase/${e}.png`}  alt={e} height='33'/>
+            </div>
+          )
+        }
+        return (
+          <div>
+          <img key={e.length} src={`smallcase/${e}.png`}  alt={e} height='20'/>
+          </div>
+        )
+      }
+})}
+</div>
+    </div>
+    </>
+  );
+}
+
+export default App;
